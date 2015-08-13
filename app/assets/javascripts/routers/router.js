bd0b51ba01fa0f515840040d1ed731cb911a5ee0
Tyler Newman <tylerjnewman18@gmail.com>
2015-08-12 17:42:01 -0700

    fix missing log on

--- app/assets/javascripts/routers/router.js
+++ app/assets/javascripts/routers/router.js
@@ -1,57 +1,53 @@
 Molyb.Routers.Router = Backbone.Router.extend({
   initialize: function (options) {
-    this.$rootEl = options.$rootEl;
-    this.$noteShowEl = $('.note-show');
-    this.$notesListEl = $('.notes-list');
+    this.$leftEl = $('.left-panel');
+    this.$midEl = $('.mid-panel');
+    this.$rightEl = $('.right-panel');
     this.notes = new Molyb.Collections.Notes();
     this.notebook = new Molyb.Collections.NoteBooks();
   },
 
   routes: {
-    "": "welcomePage",
-    "notes": "notesIndex",
+    "": "notesIndex",
     "notes/:id": "showNote",
     'notebooks/:id': 'showNotebook',
-  },
-
-  welcomePage: function () {
-    var welcomePage = new Molyb.Views.WelcomePage({});
-    this._swapView(welcomePage);
   },
 
   notesIndex: function () {
     this.notes.fetch();
     var indexView = new Molyb.Views.NotesIndex({collection: this.notes});
-    this._swapList(indexView);
+    this._swapMid(indexView);
   },
 
-  showNote: function (id) {
-    var model = this.notes.getOrFetch(id);
-    var showNoteView = new Molyb.Views.NoteShow({model: model});
-    this._swapView(showNoteView);
-  },
-
-  showNotebook: function (id) {
-    var model = this.notes.getOrFetch(id);
-    var showNotebookView = new Molyb.Views.NotebookShow({model: model});
-    this._swapView(showNotebookView);
-  },
-
-  // _swapView: function (view) {
-  //   this._currentView && this._currentView.remove();
-  //   this._currentView = view;
-  //   this.$rootEl.html(view.render().$el);
+  // showNote: function (id) {
+  //   var model = this.notes.getOrFetch(id);
+  //   var showNoteView = new Molyb.Views.NoteShow({model: model});
+  //   this._swapView(showNoteView);
   // },
 
-  _swapList: function (view) {
+  // showNotebook: function (id) {
+  //   var model = this.notes.getOrFetch(id);
+  //   var showNotebookView = new Molyb.Views.NotebookShow({model: model});
+  //   this._swapView(showNotebookView);
+  // },
+
+
+  _swapLeft: function (view) {
     this._listView && this._listView.remove();
     this._listView = view;
-    this.$notesListEl.html(view.render().$el);
+    this.$leftEl.html(view.render().$el);
   },
 
-  _swapShow: function (view) {
-    this._showView && this._showView.remove();
-    this._showView = view;
-    this.$noteShowEl.html(view.render().$el);
-  }
+  _swapMid: function (view) {
+    this._listView && this._listView.remove();
+    this._listView = view;
+    this.$leftEl.html(view.render().$el);
+  },
+
+  _swapRight: function (view) {
+    this._listView && this._listView.remove();
+    this._listView = view;
+    this.$leftEl.html(view.render().$el);
+  },
+
 });
