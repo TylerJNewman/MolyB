f858b786fedd737cfccacbfe2aa1f47da0246693
Tyler Newman <tylerjnewman18@gmail.com>
2015-08-14 14:44:27 -0700

    begin recovery

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'pages#home'
 
   namespace :api, defaults: { format: :json } do
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150813164657) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,66 +25,5 @@ ActiveRecord::Schema.define(version: 20150813164657) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-  create_table "note_tags", force: :cascade do |t|
-    t.integer  "tag_id",     null: false
-    t.integer  "note_id",    null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "note_tags", ["note_id"], name: "index_note_tags_on_note_id", using: :btree
-  add_index "note_tags", ["tag_id", "note_id"], name: "index_note_tags_on_tag_id_and_note_id", unique: true, using: :btree
-  add_index "note_tags", ["tag_id"], name: "index_note_tags_on_tag_id", using: :btree
-
-  create_table "notebooks", force: :cascade do |t|
-    t.text     "name",       null: false
-    t.integer  "owner_id",   null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "notebooks", ["owner_id"], name: "index_notebooks_on_owner_id", using: :btree
-
-  create_table "notes", force: :cascade do |t|
-    t.string   "title",       default: "Untitled"
-    t.text     "body"
-    t.integer  "owner_id",                         null: false
-    t.integer  "notebook_id",                      null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "notes", ["notebook_id"], name: "index_notes_on_notebook_id", using: :btree
-  add_index "notes", ["owner_id"], name: "index_notes_on_owner_id", using: :btree
-  add_index "notes", ["title"], name: "index_notes_on_title", using: :btree
-
-  create_table "tags", force: :cascade do |t|
-    t.text     "name",       null: false
-    t.integer  "owner_id",   null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "tags", ["owner_id"], name: "index_tags_on_owner_id", using: :btree
-
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "name"
-  end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
