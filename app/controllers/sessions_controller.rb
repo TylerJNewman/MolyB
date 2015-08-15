0feffbdcbc074b3d6ed344fed2a80aa10eb306cb
Tyler Newman <tylerjnewman18@gmail.com>
2015-08-15 15:04:11 -0700

    checkpoint

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,21 +7,27 @@ class SessionsController < ApplicationController
   end
 
   def create
-    credentials = [params[:user][:name], params[:user][:password]]
+    credentials = [params[:user][:email], params[:user][:password]]
     @user = User.find_by_credentials(*credentials)
     if @user
       login!(@user)
       redirect_to root_url
     else
-      flash.now[:notices] = ["Invalid credentials."]
-      @user = User.new
+      @user = User.new(user_params)
+      flash.now[:errors] = ["Invalid credentials."]
       render :new
     end
   end
 
   def destroy
     current_user.reset_session_token!
+    current_user = nil;
     session[:session_token] = nil
     redirect_to root_url
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
   end
 end
